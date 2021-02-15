0aafadeab0cc339180940800a9bb51dd08958966
Michael Howitz <mh@gocept.com>
2021-02-15 10:16:11 +0100

    Lint the code.
    Add support for Python 3.5, 3.6, 3.7, 3.8 and 3.9.

--- setup.py
+++ setup.py
@@ -1,41 +1,56 @@
 from setuptools import setup, find_packages
-import os.path
+
 
 version = '1.0b2'
 
-setup(name                  = "z3c.zcmlhook",
-      version               = version,
-      description           = "Easily hook into the ZCML processing machinery",
-      long_description      = open("README.txt").read() + "\n" +
-                              open(os.path.join("docs", "HISTORY.txt")).read(),
-      classifiers           = [
-            "Environment :: Web Environment",
-            "Intended Audience :: Developers",
-            "License :: OSI Approved :: Zope Public License",
-            "Programming Language :: Python",
-            "Topic :: Software Development :: Libraries :: Python Modules",
-        ],
-      keywords              = "zope zcml hook",
-      author                = "Martin Aspeli",
-      author_email          = "zope-dev@zope.org",
-      url                   = "",
-      license               = "ZPL",
-      namespace_packages    = ["z3c"],
-      packages              = find_packages("src", exclude=["ez_setup"]),
-      package_dir           = {"": "src"},
-      include_package_data  = True,
-      zip_safe              = False,
-      install_requires      = [
+
+def read(path):
+    with open(path) as f:
+        return f.read()
+
+
+setup(name="z3c.zcmlhook",
+      version=version,
+      description="Easily hook into the ZCML processing machinery",
+      long_description=(
+          read("README.rst")
+          + "\n"
+          + read('CHANGES.rst')
+      ),
+      classifiers=[
+          "Environment :: Web Environment",
+          "Intended Audience :: Developers",
+          "License :: OSI Approved :: Zope Public License",
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 2",
+          "Programming Language :: Python :: 2.7",
+          "Programming Language :: Python :: 3",
+          "Programming Language :: Python :: 3.5",
+          "Programming Language :: Python :: 3.6",
+          "Programming Language :: Python :: 3.7",
+          "Programming Language :: Python :: 3.8",
+          "Programming Language :: Python :: 3.9",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+      ],
+      keywords="zope zcml hook",
+      author="Martin Aspeli",
+      author_email="zope-dev@zope.org",
+      url="",
+      license="ZPL",
+      namespace_packages=["z3c"],
+      packages=find_packages("src"),
+      package_dir={"": "src"},
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
           "setuptools",
           "zope.component",
           "zope.interface",
           "zope.schema",
           "zope.configuration",
-          ],
-      extras_require        = dict(
+      ],
+      extras_require=dict(
           test=[
               'zope.component[test]',
-              ]),
-      tests_require         = "nose >=0.10.0b1",
-      test_suite            = "nose.collector",
+          ]),
       )
