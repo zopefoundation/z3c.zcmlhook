d38acf906a67c8f6b054b10b9bafcf8b43ea6cec
Michael Howitz <m.howitz@minddistrict.com>
2025-04-14 08:17:27 +0200

    Switch to PEP 420 native namespace. (#6)
    * Bumped version for breaking release.
    
    * Drop support for ``pkg_resources`` namespace and replace it with PEP 420 native namespace. Caution: This change requires to switch all packages in the namespace of the package to versions using a PEP 420 namespace.
    
    * Switch to PEP 420 native namespace.

--- setup.py
+++ setup.py
@@ -1,8 +1,7 @@
-from setuptools import find_packages
 from setuptools import setup
 
 
-version = '2.2.dev0'
+version = '3.0.dev0'
 
 
 def read(path):
@@ -37,9 +36,6 @@ setup(name="z3c.zcmlhook",
       author_email="zope-dev@zope.dev",
       url="https://github.com/zopefoundation/z3c.zcmlhook",
       license="ZPL",
-      namespace_packages=["z3c"],
-      packages=find_packages("src"),
-      package_dir={"": "src"},
       include_package_data=True,
       zip_safe=False,
       python_requires='>=3.9',
