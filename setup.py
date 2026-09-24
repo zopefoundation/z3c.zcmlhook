30272057e8075ff49a3d276f6ff7a7c65158075b
Michael Howitz <icemac@gmx.net>
2026-09-24 08:35:36 +0200

    Move package metadata from setup.py to pyproject.toml.

--- setup.py
+++ setup.py
@@ -1,53 +1,5 @@
 from setuptools import setup
 
 
-version = '3.1.dev0'
-
-
-def read(path):
-    with open(path) as f:
-        return f.read()
-
-
-setup(name="z3c.zcmlhook",
-      version=version,
-      description="Easily hook into the ZCML processing machinery",
-      long_description=(
-          read("README.rst")
-          + "\n"
-          + read('CHANGES.rst')
-      ),
-      classifiers=[
-          "Development Status :: 6 - Mature",
-          "Environment :: Web Environment",
-          "Intended Audience :: Developers",
-          "License :: OSI Approved :: Zope Public License",
-          "Programming Language :: Python",
-          "Programming Language :: Python :: 3",
-          "Programming Language :: Python :: 3.10",
-          "Programming Language :: Python :: 3.11",
-          "Programming Language :: Python :: 3.12",
-          "Programming Language :: Python :: 3.13",
-          "Programming Language :: Python :: 3.14",
-          "Topic :: Software Development :: Libraries :: Python Modules",
-      ],
-      keywords="zope zcml hook",
-      author="Martin Aspeli",
-      author_email="zope-dev@zope.dev",
-      url="https://github.com/zopefoundation/z3c.zcmlhook",
-      license="ZPL",
-      include_package_data=True,
-      zip_safe=False,
-      python_requires='>=3.10',
-      install_requires=[
-          "setuptools",
-          "zope.component",
-          "zope.interface",
-          "zope.schema",
-          "zope.configuration",
-      ],
-      extras_require=dict(
-          test=[
-              'zope.component[test]',
-          ]),
-      )
+# See pyproject.toml for package metadata
+setup()
